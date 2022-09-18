d00be025cfa0a162ca575e552760df071269d53a
Rico Putra Pradana <pradanaricoputra@gmail.com>
2022-09-18 14:26:45 +0700

    +T4 implement a new api POST Create Room

--- src/api/v1/RoomAPI/index.ts
+++ src/api/v1/RoomAPI/index.ts
@@ -16,6 +16,19 @@ const RoomAPI = (app: Express) => {
       })
     }
   });
+
+  app.post("/rooms", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name } = req.body;
+      const newRoom: Room = await service.createRoom(name)
+      return res.status(200).json({ newRoom })
+    }
+    catch (err: any) {
+      return res.status(500).json({
+        error: "ERROR"
+      });
+    }
+  });
 }
 
 export default RoomAPI;
--- src/repository/RoomRepository/index.ts
+++ src/repository/RoomRepository/index.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import DbClient from "../../../database/DbClient";
+import { v4 as uuidv4 } from "uuid";
+import DbClient from "../../../clients/DbClient";
 import { Room } from "../../types/Room";
 
 class RoomRepository {
@@ -18,7 +19,24 @@ class RoomRepository {
     catch (err: any) {
       throw err;
     }
-  } 
+  }
+
+  public async createRoom(name: string): Promise<Room> {
+    try {
+      const newRoom: Room = await this.dbClient.room.create({
+        data: {
+          id: "room_" + uuidv4(),
+          name,
+          members: {}
+        }
+      });
+
+      return newRoom;
+    }
+    catch (err: any) {
+      throw err;
+    }
+  }
 }
 
 export default RoomRepository;
